c951b57450ad46a5dafe90ec3b8e51cc369a9cee
lueking <lueking@csh.rit.edu>
2015-12-16 15:45:41 -0500

    claen-ups

--- day1/notquitelisp.py
+++ day1/notquitelisp.py
@@ -3,15 +3,10 @@ def main():
     pos = 0
     count = 0
     for line in open('input.txt'):
-        for word in line:
-            for char in word:
-                count += 1
-                floor += 1 if(char == "(") else -1
-                if floor < 0 and not pos == 0:
-                    print("santa is now in the basement at pos " + str(pos))
-                    pos = count
+        for char in line:
+            count += 1
+            floor += 1 if char == "(" else -1 if char == ")" else 0
+            pos = count if floor < 0 and pos == 0 else pos
     print("santa is now on floor " + str(floor))
     print("Santa hit the basement at pos " + str(pos))
 main()
-
-
